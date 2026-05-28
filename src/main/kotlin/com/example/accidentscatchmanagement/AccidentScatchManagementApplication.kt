259180da62dc13fd07af87cd99392cb2fc8fab36
Ефтимов Јорданче <jordanche.eftimov@students.finki.ukim.mk>
2026-05-28 15:00:10 +0200

    initial commit

---
+++ src/main/kotlin/com/example/accidentscatchmanagement/AccidentScatchManagementApplication.kt
@@ -0,0 +1,11 @@
+package com.example.accidentscatchmanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AccidentScatchManagementApplication
+
+fun main(args: Array<String>) {
+    runApplication<AccidentScatchManagementApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/accidentscatchmanagement/AccidentScatchManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.accidentscatchmanagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccidentScatchManagementApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
