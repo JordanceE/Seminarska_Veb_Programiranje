38b169e0b7d054a13423592cd68364691e6e4168
Jordanche Eftimov <jordanceeftimov8@gmail.com>
2026-07-18 17:34:09 +0200

    added a few adjustments

---
+++ src/main/kotlin/com/example/accidentscatchmanagement/service/AccidentSceneDeletionService.kt
@@ -0,0 +1,4 @@
+package com.example.accidentscatchmanagement.service
+
+interface AccidentSceneDeletionService {
+}
---
+++ src/main/kotlin/com/example/accidentscatchmanagement/web/AccidentSceneDeletionController.kt
@@ -0,0 +1,4 @@
+package com.example.accidentscatchmanagement.web
+
+class AccidentSceneDeletionController {
+}
